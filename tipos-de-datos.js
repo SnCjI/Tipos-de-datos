16ea2bbc7119d25d9e314b9edee751d3fe896f33
SnCjI <164548126+SnCjI@users.noreply.github.com>
2025-02-04 20:52:33 -0600

    Create tipos-de-datos.js
    commit

---
+++ tipos-de-datos.js
@@ -0,0 +1,20 @@
+// Ejemplos de tipos de datos en JavaScript
+console.log(typeof 42);          // number
+
+console.log(typeof "Veinticinco"); // string
+
+console.log(typeof -666);        // number
+
+console.log(typeof true);        // boolean
+
+console.log(typeof 0);           // number
+
+console.log(typeof '');          // string
+
+console.log(typeof null);        // object 
+
+console.log(typeof undefined);   // undefined
+
+console.log(typeof FALSE);       // undefined (FALSE no está definido, a menos que lo declares)
+
+console.log( "El tipo de datos false es:", typeof false)
